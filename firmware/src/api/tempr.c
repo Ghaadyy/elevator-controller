48bc6eb8dc7eefbed56203865167f5c00c02cca3
Antoine Karam <116711552+A-Karam20@users.noreply.github.com>
2024-12-21 20:48:00 +0200

    feat: Implemented LM35 Driver with Temperature Reading and BCD Formatting (#5)
    * feat: Implemented the lm35 driver to get the temperature
    
    * feat: Add function to get temperature in BCD format
    
    * fix: Remove useless cast when getting second digit in read_temperature_BCD_limited function

--- firmware/src/api/tempr.c
+++ firmware/src/api/tempr.c
@@ -1,12 +1,7 @@
 /**
- * @file tempr.c
- * @author Majdi Richa (majdi.richa@gmail.com)
- * @brief
- * @version 0.1
- * @date 2024-11-18
- *
- * @copyright Copyright (c) 2024
- *
+ * @file    tempr.h
+ * @author  Antoine Karam (antoinekaram1414@gmail.com)
+ * @date    2024-12-21
  */
 
 #include "../main.h"
@@ -15,5 +10,28 @@
 
 float read_temperature(void)
 {
-    return 0;
+    uint8_t adc_val;
+
+    adc_val = get_adc(ADC_TEMPR); // Read ADC
+
+    return (float)adc_val * (MAX_TEMPERATURE / (ADC_MAX_LVL - 1));
+}
+
+uint8_t read_temperature_BCD_limited(void)
+{
+    float raw_temp = read_temperature(); /* Read the temperature value */
+
+    if (raw_temp > 99)
+    { /* Limit the temperature to a maximum of 99*/
+        raw_temp = 99;
+    }
+
+    uint8_t temp = (uint8_t)raw_temp; /* Convert float to uint8_t for easier extraction*/
+
+    uint8_t first_digit = temp / 10;  /* Extract the first digit */
+    uint8_t second_digit = temp % 10; /* Extract the second digit */
+
+    uint8_t packed_digits = (second_digit & 0x0F) | ((first_digit << 4) & 0xF0); /* Pack the two digits into one byte */
+
+    return packed_digits;
 }
--- firmware/src/api/tempr.h
+++ firmware/src/api/tempr.h
@@ -1,12 +1,7 @@
 /**
- * @file tempr.h
- * @author Majdi Richa (majdi.richa@gmail.com)
- * @brief
- * @version 0.1
- * @date 2024-11-18
- *
- * @copyright Copyright (c) 2024
- *
+ * @file    tempr.h
+ * @author  Antoine Karam (antoinekaram1414@gmail.com)
+ * @date    2024-12-21
  */
 
 #ifndef TEMPR_H
@@ -14,6 +9,18 @@
 
 #define MAX_TEMPERATURE (100.0) // deg C
 
+/**
+ * @brief Get the temperature
+ *
+ * @return float
+ */
 float read_temperature(void);
+
+/**
+ * @brief Get the temperature in BCD format limited to 2 digits
+ *
+ * @return uint8_t
+ */
+uint8_t read_temperature_BCD_limited(void);
 
 #endif
